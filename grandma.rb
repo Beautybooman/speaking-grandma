513e322c22e5482095b77c56101f508ea447b5d4
Isaiah Davis <ID@Isaiahs-iMac.local>
2021-02-08 14:05:04 -0500

    commit tree e74711a05f228c7b20968b2e7acec1b35133446c

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,4 @@
 # Write a speak_to_grandma method.
-
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -11,3 +10,25 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+# require 'pry'
+
+def speak_to_grandma (phrase)
+    if phrase == phrase.downcase # if argument is all lowercase
+        'NO, NOT SINCE 1938'
+    elsif phrase == 'I LOVE YOU GRANDMA!'
+        "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase # else if argument is all caps
+        'NO, NOT SINCE 1938!'
+    else phrase == phrase.capitalize # for everything ELSE
+        "HUH?! SPEAK UP, SONNY!"  
+    end
+end
+
+
+
+# def example (phrase)
+#     phrase.upcase
+# end
+# example ('hi')
+
+# binding.pry
